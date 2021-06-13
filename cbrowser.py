db99edecac16d676b6228fff06d5720d740a95fc
nekoconn <nekoconn@gmail.com>
2021-06-13 07:20:31 +0700

    v3

--- cbrowser.py
+++ cbrowser.py
@@ -17,7 +17,6 @@ def get_files(path):
 def generate_choices(files):
     a = []
     a.append({'name': "Exit"})
-    a.append({'name': "Change directory to current..."})
     a.append({'name': "..."})
     a.append({'name': "Create new folder..."})
     a.append({'name': "Create new file..."})
@@ -40,24 +39,33 @@ def set_name():
         'message': 'Type name'
     }]
 
+def cd_to(pathh):
+    os.chdir(pathh)
+    os.system("pwd")
+    os.system("/bin/bash")
+
 class CoolBrowser(cli.Application):
-    VERSION = "2"
+    VERSION = "3"
+    ch_dir = cli.Flag(['cd'], help="Also cd to current dir when you exit")
+    homedir = cli.Flag(['home'], help="Start browser at home directory")
 
     def main(self):
-        current_path = pathlib.Path(expanduser("~"))
-
+        if (self.homedir):
+            current_path = pathlib.Path(expanduser("~"))
+        else:
+            current_path = pathlib.Path(__file__).parent.absolute()
         print_banner("Cool Browser")
         while (True):
             files = get_files(current_path)
             question = generate_question(files, current_path)
             answers = prompt(question)
             ans = answers['files']
+            selected = pathlib.Path(str(current_path) + "/" + ans)
+
             if (ans == "Exit"):
+                if (self.ch_dir):
+                    cd_to(current_path)
                 return 0
-            if (ans == "Change directory to current..."):
-                os.chdir(current_path)
-                os.system("pwd")
-                os.system("/bin/bash")
             if (ans == "..."):
                 current_path = current_path.parent.absolute()
                 continue
@@ -71,7 +79,6 @@ class CoolBrowser(cli.Application):
                 nname = prompt(name)
                 pathlib.Path(str(current_path) + "/" + nname['files']).touch()
                 continue
-            selected = pathlib.Path(str(current_path) + "/" + ans)
             if os.path.isdir(selected):
                 current_path = selected
 
@@ -83,7 +90,16 @@ if __name__ == "__main__":
 
 def test_generate_question():
     files = ["best.rb", "good.kt", "small.py"]
-    question = generate_question(files)
+    question = generate_question(files,"")
     assert len(question) == 1, "has to be one question"
     assert question[0]['type'] == 'select', "only 1 option is allowed"
     assert len(question[0]['choices']) == len(files) + 4, "same number of choices as files plus 4 options"
+
+def test_cd_to():
+    current_path = pathlib.Path(os.getcwd())
+    filepath = current_path
+
+    cd_to(current_path.parent.absolute())
+    current_path = os.getcwd()
+
+    assert current_path != filepath
